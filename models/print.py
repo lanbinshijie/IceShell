e1fe35e6592d37acab0845ca8f20e699d69e0e44
lanbinshijie <huoziye123@163.com>
2023-04-13 21:33:24 +0800

    feat: 更新了print命令

--- models/print.py
+++ models/print.py
@@ -1,1 +1,6 @@
-print("Helloworld!")
+from argparse import ArgumentParser
+parser = ArgumentParser()
+parser.add_argument("echo", help="add some")
+args = parser.parse_args()
+echo = args.echo
+print(echo)
