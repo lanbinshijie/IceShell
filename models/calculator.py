77db8f4460a7d40ad6a7244d2e365039d2f216e0
lanbinshijie <huoziye123@163.com>
2024-01-07 00:12:17 +0800

    feat: 增加了插件元信息适配

--- models/calculator.py
+++ models/calculator.py
@@ -1,3 +1,10 @@
+#! IcePlugin.v1
+#! PluginName: Calculator
+#! Author: Lanbin
+#! Description: 一个简单的计算器
+#! Version: 1.0.0
+#! LastEdit: 2024-1-7 00:05:41
+
 while True:
     a = input(">>> ")
     if a == "q":
