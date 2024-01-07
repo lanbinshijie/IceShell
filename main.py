0e519803a90869ed2a785f1fcc5bc8c1c2c38d2c
lanbinshijie <huoziye123@163.com>
2024-01-07 18:45:40 +0800

    feat: 新增用户配置

--- main.py
+++ main.py
@@ -14,9 +14,14 @@ from misc.Error import Error
 from tools.Phraser import PS1
 from tools.Phraser import alias
 from misc.Info import StartAction
+from tools.Configure import *
 # from models.bash import Ish
 
+
+
 ALIAS = alias()
+PS1O = PS1()
+CONFIGURATION = Configuration()
 # ish = Ish()
 
 def ExecuteModel(args, moduleName):
@@ -38,13 +43,58 @@ def ExecuteModel(args, moduleName):
         # 因为 checkModel 会给出提示信息，所以就不用给出了
         ...
 
+def SafetyMode(lock):
+    if int(lock):
+        print(Colors.RED + Logo.div_line_n_m + Colors.END)
+        print(Colors.RED + "Safety Mode is on." + Colors.END)
+        print(Colors.RED + "Please input your password to continue." + Colors.END)
+        print(Colors.RED + Logo.div_line_n_m + Colors.END + "\n")
+
+        while True:
+            try:
+                userName = input("Username: >> ")
+                password = input("Password: >> ")
+                passwordHashed = Hash(password)
+                if userName == CONFIGURATION.userName and passwordHashed == str(CONFIGURATION.passwordHashed).split("%%")[0]:
+                    print()
+                    print(Colors.GREEN + Logo.div_line_n_m + Colors.END)
+                    print(Colors.GREEN + "Welcome back, " + userName + "!" + Colors.END)
+                    print(Colors.GREEN + Logo.div_line_n_m + Colors.END + "\n")
+                    break
+                else:
+                    print()
+                    print(Colors.RED + Logo.div_line_n_m + Colors.END)
+                    print(Colors.RED + "Wrong username or password!" + Colors.END)
+                    print(Colors.RED + Logo.div_line_n_m + Colors.END + "\n")
+                    continue
+            except KeyboardInterrupt:
+                print("\nBye~")
+                exit(0)
+
 def IceShell():
     extra = ""
     commandN = input(Colors.RED + extra + PS1.paraphraser() + Colors.END)
     command = commandN.split(" ")
+    SECLEVEL = 0
     if command[0] == "q":
         print("Bye~")
         exit(0)
+    elif CONFIGURATION.ifexist(str(commandN.split("=")[0]).strip()[1:]) and commandN.startswith("$"):
+        conf = commandN.split("=")
+        conf = [str(item).strip() for item in conf]
+        conf[0] = conf[0][1:]
+        sec = CONFIGURATION.get(conf[0]).split("%%") # 安全等级，如果越大，需要的等级越高
+        sec = int(sec[1]) if len(sec) > 1 else 0
+        if sec > SECLEVEL:
+            Error.printError(30001)
+            print()
+            return
+        if len(conf) == 2:
+            CONFIGURATION.set_value(conf[0], conf[1])
+            CONFIGURATION.save()
+            print(Colors.GREEN + "Configuration saved." + Colors.END)
+        elif len(conf) == 1:
+            print(CONFIGURATION.get(conf[0]))
     elif command[0] in ProgramInfo.registered_modules or "*" in ProgramInfo.registered_modules:
         ExecuteModel(" ".join(command[1:]), command[0])
     else:
@@ -62,7 +112,7 @@ if __name__ == "__main__":
     if os.path.exists(mfol_stp):
         ExecuteModel(mdls_stp, "bash")
     print(Colors.BLUE + Logo.div_line_n_m + Colors.END + "\n")
-
+    pass_ = SafetyMode(CONFIGURATION.lock)
     while True:
         try:
             IceShell()
